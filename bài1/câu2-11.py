5602ac83da39552036d9bbd6233d9a99b5645fb7
ngodaccuong311006-stack <ngodaccuong311006@gmail.com>
2025-12-14 17:36:58 +0700

    Add files via upload

---
+++ bài1/câu2-11.py
@@ -0,0 +1,13 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+
+l = [1, 'python', 4, 7]
+k = ['cse', 2, 'guntur', 8]
+m = []
+
+m.append(l)
+m.append(k)
+print(m)
+
+d = {1: l, 2: k, 'combine_list': m}
+print(d)
