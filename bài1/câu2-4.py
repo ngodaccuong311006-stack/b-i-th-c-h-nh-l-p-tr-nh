a14be1e15938ae2ba7c080a6d06e05cd1e40a3a4
ngodaccuong311006-stack <ngodaccuong311006@gmail.com>
2025-12-14 17:08:56 +0700

    Add files via upload

---
+++ bài1/câu2-4.py
@@ -0,0 +1,8 @@
+print("sinh viên: ngô đắc cường")
+print("msv: 245752021610035")
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+for i in range(a + 1, b):
+    print("1 /", i, "=", 1 / i)
